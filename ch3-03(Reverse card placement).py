34df3791128032d81a6c6eba14b4b506ff3237f3
jejeon98 <65063839+jejeon98@users.noreply.github.com>
2021-09-01 01:16:28 +0900

    Add files via upload

---
+++ ch3-03(Reverse card placement).py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin=open("input.TXT","rt")
+card=[0]*20
+for i in range(20):
+    card[i]=(i+1)
+for i in range(10):
+    a,b=map(int,input().split())
+    for j in range(((b-a)+1)//2):
+        card[j+a-1],card[b-j-1]=card[b-j-1],card[j+a-1]
+for a in card:
+    print(a,end=' ')
+
+
+
+
+
+        
+
+
